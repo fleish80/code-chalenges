3e65842063186c180364978aaf12315b133d19f1
DXF0530 <dmitry.fleish@sbdinc.com>
2022-11-16 21:44:44 +0200

    findNumberOfIsolatedStrings

---
+++ packages/main/find-number-of-isolated-strings/index.ts
@@ -0,0 +1,22 @@
+const findNumberOfIsolatedStrings = (str: string): number => {
+  let counter = 0;
+  const set = new Set<string>();
+  if (str) {
+    for (let i = 0; i < str.length; i++) {
+      if (set.has(str[i])) {
+        counter++;
+        set.clear();
+      }
+      set.add(str[i]);
+    }
+    counter++;
+  }
+  return counter;
+}
+
+(() => {
+  document.body.append(`<p>abcbabcxvgc-${findNumberOfIsolatedStrings('abcbabcxvgc')}</p>`);
+  document.body.append(`<p>aaaaabcbabcxvgc-${findNumberOfIsolatedStrings('aaaaabcbabcxvgc')}</p>`);
+  document.body.append(`<p>-${findNumberOfIsolatedStrings('')}</p>`);
+})();
+
