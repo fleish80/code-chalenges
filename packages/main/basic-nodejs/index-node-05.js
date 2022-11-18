5070d77fd051b34c4cd9f920d067cccc447cfcf7
DXF0530 <dmitry.fleish@sbdinc.com>
2022-11-18 10:07:12 +0200

    remove basic node

--- packages/main/basic-nodejs/index-node-05.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const path = require("path");
-
-const resolvePath = path.resolve(__dirname, 'files\\input.txt');
-
-console.log('resolve', resolvePath);
-let textIn = fs.readdirSync(resolvePath);
-
-console.log(textIn);
